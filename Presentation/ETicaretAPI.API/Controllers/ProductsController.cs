2026e064bb39b59b758f8de4cfb09eafd803f5dc
nurioget <nurioget9@gmail.com>
2024-06-05 17:53:21 +0300

    I added Generic Repository Design Pattern

--- Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 ﻿
+using ETicaretAPI.Application.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,8 +9,25 @@ namespace ETicaretAPI.API.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
-     
+        private readonly IProductWiriteRepository  _productWiriteRepository;
+        private readonly IProductReadRepository _productReadRepository;
 
-    
+        public ProductsController(IProductWiriteRepository productWiriteRepository, IProductReadRepository productReadRepository)
+        {
+            _productWiriteRepository = productWiriteRepository;
+            _productReadRepository = productReadRepository;
+        }
+
+        [HttpGet]
+        public async void Get()
+        {
+           await _productWiriteRepository.AddRangeAsync(new()
+            {
+                new() { Id=Guid.NewGuid(),Name="Product 1",Price=100,CreateDate=DateTime.UtcNow,Stock=10},
+                new() { Id=Guid.NewGuid(),Name="Product 2",Price=234,CreateDate=DateTime.UtcNow,Stock=23},
+                new() { Id=Guid.NewGuid(),Name="Product 3",Price=5432,CreateDate=DateTime.UtcNow,Stock=34}
+                });
+           await _productWiriteRepository.SaveAsync();
+        }
     }
 }
