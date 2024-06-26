c80d8a2617bf17abf7635fa2bf570b5ecab4cbbf
nurioget <nurioget9@gmail.com>
2024-06-26 13:35:56 +0300

    program.cs fix

---
+++ Presentation/ETicaretAPI.API/Program.cs
@@ -0,0 +1,40 @@
+using ETicaretAPI.Application.Validators.Products;
+using ETicaretAPI.Infrastructure;
+using ETicaretAPI.Infrastructure.Filters;
+using ETicaretAPI.Persistence;
+using FluentValidation.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddPersistenceServices();
+builder.Services.AddInfrastructureServices();
+builder.Services.AddCors(options => options.AddDefaultPolicy(policy =>
+    policy.WithOrigins("http://localhost:4200", "https://localhost:4200").AllowAnyHeader().AllowAnyMethod()
+    ));
+
+builder.Services.AddControllers(options=>options.Filters.Add<ValidationFilter>())
+    .AddFluentValidation(configuration=>configuration.RegisterValidatorsFromAssemblyContaining<CreateProductValidator>())
+    .ConfigureApiBehaviorOptions(options=>options.SuppressModelStateInvalidFilter=true);
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(); 
+}
+
+app.UseStaticFiles();
+app.UseCors();
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
