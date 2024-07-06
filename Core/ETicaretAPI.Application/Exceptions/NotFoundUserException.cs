766401e8070515084aa37a49435b8d89c47e20b0
nurioget <nurioget9@gmail.com>
2024-07-06 11:20:44 +0300

    User Login (Authentication) Operations

---
+++ Core/ETicaretAPI.Application/Exceptions/NotFoundUserException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Exceptions
+{
+    public class NotFoundUserException : Exception
+    {
+        public NotFoundUserException() :base("Kullanıcı adı veya şifre hatalı") 
+        {
+        }
+
+        public NotFoundUserException(string? message) : base(message)
+        {
+        }
+
+        public NotFoundUserException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+    }
+}
---
+++ Core/ETicaretAPI.Application/Features/Commands/AppUser/LoginUser/LoginUserCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using ETicaretAPI.Application.Exceptions;
+using ETicaretAPI.Domain.Entities.Identity;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Features.Commands.AppUser.LoginUser
+{
+    public class LoginUserCommandHandler : IRequestHandler<LoginUserCommandRequest, LoginUserComandResponse>
+    {
+        private readonly UserManager<Domain.Entities.Identity.AppUser> _userManager;
+        private readonly SignInManager<Domain.Entities.Identity.AppUser> _signManager;
+
+        public LoginUserCommandHandler(UserManager<Domain.Entities.Identity.AppUser> userManager, SignInManager<Domain.Entities.Identity.AppUser> signManager)
+        {
+            _userManager = userManager;
+            _signManager = signManager;
+        }
+
+        public async Task<LoginUserComandResponse> Handle(LoginUserCommandRequest request, CancellationToken cancellationToken)
+        {
+            Domain.Entities.Identity.AppUser user = await _userManager.FindByNameAsync(request.UsernameOrEmail);
+            if (user is null)
+                user = await _userManager.FindByNameAsync(request.UsernameOrEmail);
+
+            if (user is null)
+                throw new NotFoundUserException("Kullanıcı adı veya Şifre hatalı");
+
+            SignInResult result = await _signManager.CheckPasswordSignInAsync(user, request.Password, false);
+            if (result.Succeeded)//Authentication Succeeded
+            {
+                //yetki belirlenmeli
+            }
+                return new();
+
+        }
+    }
+}
